ec3af83182ba220db54d84108a7bc26911a876f7
Dhruv T <36724675+L05Dhruv@users.noreply.github.com>
2024-08-15 01:10:59 +0000

    fixing errors

--- problems/pb2.py
+++ problems/pb2.py
@@ -1,4 +1,4 @@
 # declare and fill out function here
 
 # test case
-print(max_values2([-5, -2, -1, -11])) # -> [1, 2]
+print(max_values([-5, -2, -1, -11])) # -> [1, 2]
--- problems/pb3.py
+++ problems/pb3.py
@@ -4,4 +4,4 @@
 
 # test case
 students = {"Drake": 21, "Alice": 18, "Bob": 20, "Charlie": 19, "David": 22, "Jay": 20}
-print(youngest_student2(students))  # Expected output: "Alice"
+print(youngest_student(students))  # Expected output: "Alice"
